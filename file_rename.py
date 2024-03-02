988f5099d8d3d71902deb58f85b69b195daadc4f
Muffinz <david.kanovsky7@gmail.com>
2024-03-02 18:03:40 +0100

    Update file_rename.py

--- file_rename.py
+++ file_rename.py
@@ -1,11 +1,9 @@
 import os
 
 def rename_files(folder_path):
-    # Get the list of files in the folder
     files = os.listdir(folder_path)
     
     for file_name in files:
-        # Check if the file name contains the specified prefix
         if 'SpotMate.online - ' in file_name:
             # Construct the new file name by removing the prefix
             new_file_name = file_name.replace('SpotMate.online - ', '')
@@ -19,5 +17,4 @@ def rename_files(folder_path):
 # Replace 'folder_path' with the path to your folder
 folder_path = 'C:/Users/kanov/Music/techno'
 
-# Call the function to rename files in the specified folder
 rename_files(folder_path)
